13015e2947d92f8339909f09c5c38b54fc12f652
Gabriel Sampaio <gabriel.antoniosampaio@gmail.com>
2021-11-19 16:19:32 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { useFonts } from 'expo-font';
+import {
+  RedHatDisplay_400Regular,
+  RedHatDisplay_500Medium,
+  RedHatDisplay_700Bold,
+} from '@expo-google-fonts/red-hat-display';
+import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+import { Routes } from './src/routes';
+import { Background } from './src/components/Background';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    RedHatDisplay_400Regular,
+    RedHatDisplay_500Medium,
+    RedHatDisplay_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </Background>
+  );
+}
+
